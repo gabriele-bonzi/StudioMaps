c9ad74e4275e0ef69f1fcba155fdd1d3f422b0d8
gabrielebonzi <gabriele.bonzi@studiofarma.it>
2013-05-08 09:24:27 +0200

    SND

---
+++ StudioMaps.API/Hubs/TrackPositionHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace StudioMaps.API.Hubs
+{
+    public class TrackPositionHub : Hub
+    {
+         public void Send(string message)
+         {
+             Clients.All.SetPositionMarker(message);
+         }
+    }
+}
